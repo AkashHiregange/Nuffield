073e66324b2648b693b9c92e4a1ec7cf3785e553
Merlin Warner-Huish <mwarnerhuish@gmail.com>
2023-08-11 14:38:13 +0100

    created functions

--- PythonFiles/charges.py
+++ PythonFiles/charges.py
@@ -1,3 +1,4 @@
+#IMPORTS
 import numpy as np
 from ase.phonons import Phonons
 from phonopy import Phonopy
@@ -13,57 +14,61 @@ import shutil
 
 from phonopy.interface.calculator import read_crystal_structure, write_crystal_structure
 
+
+num1 = int(input("What dimension would you like your supercell to be? "))
+num2 = float(input("What displacement would you like to use? "))
+
+
+#DEFINING VARIABLES
 unitcell, optional_structure_info = read_crystal_structure("geometry.in", interface_mode='aims')
 
-sup_matrix = np.array([[2, 0, 0], [0, 2, 0], [0, 0, 2]])
-det = np.round(np.linalg.det(sup_matrix))
-print(np.round(det))
-phonon = Phonopy(unitcell,
-                 supercell_matrix=sup_matrix)
-
-phonon.generate_displacements(distance=0.01)
-supercells = phonon.supercells_with_displacements
-
-# print(supercells[0])
 
 
 crys = read("geometry.in")
-print(len(crys))
-charges = crys.get_initial_charges()
 
-print(charges)
+sup_matrix = np.array([[num1, 0, 0], [0, num1, 0], [0, 0, num1]])
+det = np.round(np.linalg.det(sup_matrix))
+phonon = Phonopy(unitcell, supercell_matrix=sup_matrix)
 
-charges2 = []
-print(type(charges2))
-# print(charges3)
-
-moments = crys.get_initial_magnetic_moments()
-moments2 = []
-
-for i in range(len(crys)):
-    for j in range(int(det)):
-        charges2.append(charges[i])
-        moments2.append(moments[i])
+phonon.generate_displacements(distance=num2)
+supercells = phonon.supercells_with_displacements
 
 
-print(len(charges2))
 
-chem_sym = crys.get_chemical_symbols()
+def get_charges_and_moments(determinant, atoms_object):
+    charges2 = []
+    moments2 = []
 
-for ind, sup in enumerate(supercells):
-    write_crystal_structure(f"geomtry_{ind}.in", supercells[ind], interface_mode='aims')
-    atoms = read(f"geomtry_{ind}.in")
-    atoms.set_initial_charges(charges2)
-    atoms.set_initial_magnetic_moments(moments2)
-    directory = f"disp_{ind}"
-    parent_dir = "C:/Users/Merlin Warner-Huish/PycharmProjects/Merlin/"
-    path_final = os.path.join(parent_dir, directory)
-    if os.path.exists(path_final):
-        shutil.rmtree(path_final)
-        os.mkdir(path_final)
-        atoms.write(path_final + '/geometry.in')
-    else:
-        os.mkdir(path_final)
-        atoms.write(path_final + '/geometry.in')
-    os.remove(f"geomtry_{ind}.in")
+    charges = atoms_object.get_initial_charges()
+    moments = atoms_object.get_initial_magnetic_moments()
+
+    for i in range(len(atoms_object)):
+        for j in range(int(determinant)):
+            charges2.append(charges[i])
+            moments2.append(moments[i])
+
+    return moments2, charges2
+def creating_files_and_directories(atoms_object, charges, moments):
+    chem_sym = atoms_object.get_chemical_symbols()
+    for ind, sup in enumerate(supercells):
+        write_crystal_structure(f"geometry_{ind}.in", supercells[ind], interface_mode='aims')
+        atoms = read(f"geometry_{ind}.in")
+        atoms.set_initial_charges(charges)
+        atoms.set_initial_magnetic_moments(moments)
+        directory = f"disp_{ind}"
+        parent_dir = os.getcwd()
+        path_final = os.path.join(parent_dir, directory)
+        if os.path.exists(path_final):
+            shutil.rmtree(path_final)
+            os.mkdir(path_final)
+            atoms.write(path_final + '/geometry.in')
+        else:
+            os.mkdir(path_final)
+            atoms.write(path_final + '/geometry.in')
+        os.remove(f"geometry_{ind}.in")
+
+charges = get_charges_and_moments(det, crys)[1]
+moments = get_charges_and_moments(det,crys)[0]
+get_charges_and_moments(det, crys)
+creating_files_and_directories(crys, charges, moments)
 
